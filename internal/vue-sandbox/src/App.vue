f6c52560a10a473e3fc35cba532400f18d6a4d11
Martin Carlsson <martin.carlsson@forsakringskassan.se>
2024-10-18 10:59:30 +0200

    WIP

--- internal/vue-sandbox/src/App.vue
+++ internal/vue-sandbox/src/App.vue
@@ -1,3 +1,26 @@
+<script setup lang="ts">
+import { onBeforeMount, ref, nextTick } from "vue";
+import { compileScript, parse } from "vue/compiler-sfc";
+import sourceCode from "./test.vue?raw";
+
+const test = parse(sourceCode);
+const compiledScript = compileScript(test.descriptor, { id: "a" });
+const encoded = btoa(compiledScript.content);
+const datauri = `data:text/javascript;base64,${encoded}`;
+const AwesomeComponent = ref(undefined);
+const ready = ref(false);
+
+onBeforeMount(async () => {
+    AwesomeComponent.value = await import(/* @vite-ignore */ datauri);
+    await nextTick();
+    await nextTick();
+    await nextTick();
+    ready.value = true;
+});
+</script>
+
 <template>
-    <router-view />
+    <div v-if="ready">
+        <component :is="AwesomeComponent"></component>
+    </div>
 </template>
---
+++ internal/vue-sandbox/src/test.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const myName = ref("Composition  API!!!!!!!!");
+</script>
+
+<template>
+    <p>Hello {{ myName }}</p>
+</template>
